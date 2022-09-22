35d81d0b16d94ea664babdb0e32abf4ff2a66b48
evandamanggani5 <114144824+evandamanggani5@users.noreply.github.com>
2022-09-22 16:20:53 +0700

    pratikum1
    hasil pratikum 1

---
+++ latihan 2.py
@@ -0,0 +1,9 @@
+print("hitung luas ruangan")
+P= int(input("Masukan Panjang Ruangan:"))
+L= int(input("Maaukan Rebar Ruangan:"))
+S= input("Masukan Satuan(Meter/Inci):")
+X= P*L
+p2= str(P)
+l2= str(L)
+x2= str(X) 
+print("luas ruangan dengan panjang "+p2+" dan lebar "+l2+" adalah "+ x2 +""+S)
---
+++ latihan satu.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+x= input(" what is your name:")
+y= input(" where are you from:")
+a= input(" where did you go to school before:")
+print("hello," + x )
+print("waw, from bangka belitung," + y)
+print("it must be fun to study in bangka belitung," + a)
